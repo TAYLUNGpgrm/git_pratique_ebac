5406ee7098a4abca3155fc830d31dd0d245f7337
Alef Dutra <alefdutran@gmail.com>
2024-08-26 13:25:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function botao1() {
+    alert("Que bom que Gostou!😁 Agradecemos seu FeedBack.");
+    let email = prompt("Talvez, você queira nos contar mais sobre oque gostou em nosso site?¯\_( ͡° ͜ʖ ͡°)_/¯");
+}
+function botao2() {
+    alert("Hmm...Que pena,🫤 seu FeedBack ajuda bastante, agradecemos!😊");
+    let nome = prompt('Digite aqui, o motivo de sua insatisfação pelo conteúdo mostrado.');
+    alert("Obrigado pela avaliação do nosso conteúdo, é bastante importante para o crescimento do nosso site!(>'-'<)")
+}
